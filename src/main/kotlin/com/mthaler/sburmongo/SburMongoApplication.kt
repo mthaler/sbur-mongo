06415c626a50f99391655fb1c1ef8ea9c53eeafd
Michael Thaler <michael.thaler@dlr.de>
2021-06-13 13:56:21 +0200

    Added SburMongoApplication.

---
+++ src/main/kotlin/com/mthaler/sburmongo/SburMongoApplication.kt
@@ -0,0 +1,11 @@
+package com.mthaler.sburmongo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SburMongoApplication
+
+fun main(args: Array<String>) {
+    runApplication<SburMongoApplication>(*args)
+}
